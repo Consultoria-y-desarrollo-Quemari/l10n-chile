44e271555c666ffea3ba1ffb21ae73afec74b536
Ernesto García Medina <egarcia@quemari.com>
2020-06-29 13:28:06 -0500

    changes to fix problem import res_company

--- l10n_cl_etd/models/res_company.py
+++ l10n_cl_etd/models/res_company.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2019 CubicERP
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, models, _
+from odoo import api, models, _, fields
 from odoo.exceptions import ValidationError
 
 

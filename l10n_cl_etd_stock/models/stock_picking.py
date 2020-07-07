fdf59f8bda805bd9b8b4ab39cc1e54da61584ad2
Luis Ernesto García Medina <ernesto.r.2.em@gmail.com>
2020-07-07 15:48:10 -0500

    Update stock_picking.py

--- l10n_cl_etd_stock/models/stock_picking.py
+++ l10n_cl_etd_stock/models/stock_picking.py
@@ -5,6 +5,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, models, fields, _
 
+import logging
+
+_logger = logging.getLogger(__name__)
+
 try:
     from io import BytesIO
 except:
